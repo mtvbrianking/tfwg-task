102a9799e7ba3fc1d70b16650356eb75db825fd3
mtvbrianking <mtvbrianking@gmail.com>
2025-04-22 12:34:54 +0300

    CRUD: Get client

--- api/src/controllers/clients.ts
+++ api/src/controllers/clients.ts
@@ -8,3 +8,16 @@ export const getClients = async (req: Request, res: Response): Promise<void> => 
 
     res.json({ clients: clients });
 };
+
+export const getClient = async (req: Request, res: Response): Promise<void> => {
+    const id = parseInt(req.params.id);
+
+    const client = await prisma.client.findUnique({ where: { id } });
+
+    if (!client) {
+        res.status(404).json({ message: `Client with ID ${id} was not found.` });
+        return;
+    }
+
+    res.json(client);
+};
--- api/src/routes/clients.ts
+++ api/src/routes/clients.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import {
-    getClients
+    getClients,
+    getClient
 } from '../controllers/clients';
 
 const router = express.Router();
 
 router.get('/', getClients);
+router.get('/:id', getClient);
 
 export default router;
