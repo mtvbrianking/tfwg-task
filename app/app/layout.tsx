ac09550e126ab1be70a67e9dfaf03bbdf4752b9f
mtvbrianking <mtvbrianking@gmail.com>
2025-04-22 18:09:49 +0300

    layout realignment

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,8 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,26 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          {/* <AppSidebar variant="inset" /> */}
+          <SidebarInset>
+            <SiteHeader />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 p-7 md:gap-6 md:py-6">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,8 +1,4 @@
 import axios from "axios"
-import { AppSidebar } from "@/components/app-sidebar"
-import { SiteHeader } from "@/components/site-header"
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
-
 import { columns } from "./components/columns"
 import { DataTable } from "./components/data-table"
 
@@ -21,25 +17,6 @@ export default async function Page() {
   const clients = await fetchClients();
 
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      {/* <AppSidebar variant="inset" /> */}
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 p-7 md:gap-6 md:py-6">
-              <DataTable data={clients} columns={columns} />
-            </div>
-          </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <DataTable data={clients} columns={columns} />
   )
 }
